801f87cf80717436c806f5b2f615791324ecf154
clement maimane <tshwaraganangmaimane25@gmail.com>
2024-11-06 09:09:30 +0200

    ADDING REMOVE BUTTON TO MY TODO

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-import express from 'express'; 
+import express from 'express';
+
 const app = express();
 const port = 3000;
 
@@ -9,13 +10,20 @@ app.use(express.urlencoded({ extended: true }));
 let tasks = [];
 
 app.get('/', (req, res) => {
-    res.render('index', { tasks });
+    res.render('index', { tasks });  // Render the tasks on the index page
 });
 
 app.post('/add', (req, res) => {
     const { task } = req.body;  
-    tasks.push(task);
-    res.redirect('/');
+    tasks.push(task);  // Add new task to the tasks array
+    res.redirect('/');  // Redirect back to home
 });
+
+app.post('/remove', (req, res) => {
+    const taskToRemove = req.body.task;  // Get the task to remove from the form input
+    tasks = tasks.filter(task => task !== taskToRemove);  // Filter out the task
+    res.redirect('/');  // Redirect back to the home page
+});
+
 // Start the server
 app.listen(port, () => console.log(`Server is running on port ${port}`));
